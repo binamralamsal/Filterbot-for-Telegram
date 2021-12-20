b044c2c372cba6fdb7496a1dc739be497afb524e
YaswanthKumar12345 <95373055+YaswanthKumar12345@users.noreply.github.com>
2021-12-20 16:38:06 +0530

    Update start.js

--- controllers/utils/start.js
+++ controllers/utils/start.js
@@ -6,6 +6,6 @@ export default async (message) => {
 
   await bot.sendMessage(
     message.chat.id,
-    "I can send you your movies, Please join our group"
+    "Join @TJMANforever for JEE/NEET preaparation and books"
   );
 };
